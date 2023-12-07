26e50b987b9fa8df84add755b42f5f22cde9bc1e
Shubham Kumar Vishwakarma <shubhamv387@gmail.com>
2023-12-07 11:10:44 +0530

    automated dynamic routing using Next/Link

--- first/app/aboutus/[id]/page.js
+++ first/app/aboutus/[id]/page.js
@@ -1,8 +1,4 @@
-const details = [
-  { id: 1, name: 'Yash', role: 'Senior Developer' },
-  { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
-  { id: 3, name: 'Suresh', role: 'Frontend Developer' },
-];
+import { details } from '@/app/constants/users';
 
 export default function Details({ params }) {
   const { id } = params;
--- first/app/aboutus/page.js
+++ first/app/aboutus/page.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { details } from '@/app/constants/users';
+import Link from 'next/link';
 
 const About = () => {
-  return <h1>About Us Page</h1>;
+  return (
+    <>
+      <h1>About Us Page</h1>
+      {details.map((user) => (
+        <div>
+          <Link href={`aboutus/${user.id}`} key={user.id}>
+            {user.name}{' '}
+          </Link>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default About;
